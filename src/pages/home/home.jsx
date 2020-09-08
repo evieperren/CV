8c926a1fcdec806222bc28c344781ca2dfd26f5c
Evie Perren <evie.perren@simplyhealth.co.uk>
2020-09-08 23:57:36 +0300

    styling home page header

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,17 +1,63 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 import { Link } from "react-router-dom"
 import Menu from '../../components/menu/menu'
 import styles from './home.module.scss'
+import sky from '../../assets/images/sky.jpg'
+import street from '../../assets/images/street.jpg'
 
 const HomePage = () => {
+  const [ showFeatureImg, setShowFeatureImg] = useState(false)
+
+  function handleMouseOver(){
+    setShowFeatureImg(true)
+  }
+  function handleMouseLeave(){
+    setShowFeatureImg(false)
+
+  }
   return(
       <div className={styles.container}>
-      <header>
-        <Menu></Menu>
-        <Link to="/work" className={styles.non_menu_link}>Work</Link>
-      </header>
-        <h1><span className={styles.float__left}>EVIE</span><span className={styles.float__right}>PERREN</span></h1>
-        <p>Evie is a software developer, focusing on front-end development</p>
+        <header>
+          <Menu></Menu>
+          <Link to="/work" className={styles.non_menu_link}>Work</Link>
+        </header>
+        {showFeatureImg && (
+          <Fragment>
+            <img className={styles.img__top} src={sky}/>
+            <span className={styles.img__reference}>Photo by 
+              <a href="https://unsplash.com/@kevinmueller?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
+                Kevin Mueller</a> on 
+                <a href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash
+              </a>
+            </span>
+          </Fragment>
+        )}
+        <h1 onMouseEnter={handleMouseOver} onMouseLeave={handleMouseLeave}>
+          <span className={styles.float__left}>Evie</span>
+          <span className={styles.float__right}>Perren</span>
+        </h1>
+        <p className={styles.feature__bottom}></p>
+        {showFeatureImg && (
+          <Fragment>
+            <img className={styles.img__bottom} src={street} alt=""/>
+            <span className={styles.img__reference}>Photo by 
+              <a href="https://unsplash.com/@pelayoarbues?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Pelayo Arbués</a> on 
+              <a href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a>
+            </span>
+          </Fragment>
+        )}
+
+
+
+
+
+
+
+
+        <div className={styles.filler_container}>
+          <p>This should be a filler white space</p>
+        </div>
+
       </div>
   )
 }
