9f2762846bc37cad9246822efa56e70e85aa80b5
Evie Perren <evie.perren@simplyhealth.co.uk>
2020-09-14 20:40:13 +0100

    getting state to change correctly

---
+++ src/components/carousel/carousel.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react'
+import styles from './carousel.module.scss'
+import hr_project_laptop from '../../assets/images/hr-desktop.png'
+import self_serve_tablet from '../../assets/images/self-serve.png'
+import pet_checkout_mobile from '../../assets/images/pets-checkout-mobile.png'
+import pet_link_laptop from '../../assets/images/pets-link-generator.png'
+
+const Carousel = () => {
+  const [ activeIndex, setActiveIndex ] = useState(0)
+
+  const slides = [
+    {
+      title: 'HR Mentor Project',
+      image: hr_project_laptop,
+      class: styles.work__img1,
+    },
+    {
+      title: 'Pet Checkout',
+      image: pet_checkout_mobile,
+      class: styles.work__img1
+    },
+    {
+      title: 'Self Service Portal Redesign',
+      image: self_serve_tablet,
+      class: styles.work__img1
+    },
+    {
+      title: 'Link Generator',
+      image: pet_link_laptop,
+      class: styles.work__img2
+    }
+  ]
+  function goToPreviousSlide(){
+    if(activeIndex < 1){
+      setActiveIndex(slides.length)
+    } else (
+      setActiveIndex(activeIndex - 1)
+    )
+  }
+  function goToNextSlide(){
+    if(activeIndex < slides.length){
+      setActiveIndex(activeIndex + 1)
+    } 
+    else (
+      setActiveIndex(0)
+    )
+  }
+  console.log(`active index: ${activeIndex}`)
+  return (
+    <div className={styles.slider}>
+      <div className={styles.items}>
+        <span className={styles.scroll_left} aria-label="scroll left" onClick={goToPreviousSlide}></span>
+        <div className={styles.item}>
+          <ul>
+            {slides.map((item, key) => 
+            { return (
+              <li key={key}>
+                <a href="/">
+                  <p>{item.title}</p> 
+                  <img src={item.image} alt={item.title} className={styles.work__img1}/>
+                </a>
+              </li>)
+            })}
+          </ul>
+        </div>
+        <span className={styles.scroll_right} aria-label="scroll right" onClick={goToNextSlide}></span>
+      </div>
+    </div>
+  )
+}
+export default Carousel
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -4,10 +4,7 @@ import Menu from '../../components/menu/menu'
 import styles from './home.module.scss'
 import sky from '../../assets/images/sky.jpg'
 import street from '../../assets/images/street.jpg'
-import hr_project_laptop from '../../assets/images/hr-desktop.png'
-import self_serve_tablet from '../../assets/images/self-serve.png'
-import down_arrow from '../../assets/images/angle-double-down-solid.svg'
-import pet_checkout_mobile from '../../assets/images/pets-checkout-mobile.png'
+import Carousel from '../../components/carousel/carousel'
 
 const HomePage = () => {
   const [ showFeatureImg, setShowFeatureImg] = useState(false)
@@ -67,33 +64,7 @@ const HomePage = () => {
         <div id="work" className={styles.work}>
           <h2>Works</h2>
           <h3 className={styles.work__number}>0{workNumber}</h3>
-          <ul className={styles.work__list}>
-            <li className={styles.work__list_item}>
-              <a href="/">
-                <p>HR Mentor Project</p>
-                <img src={hr_project_laptop} alt="HR mentor project" className={styles.work__img1}/>
-              </a>
-              <span className={styles.scroll} aria-label="scroll"></span>
-            </li>
-            <li className={styles.work__list_item}>
-              <a href="/">
-                <p>Self-service portal redesign</p>
-                <img src={self_serve_tablet} alt="Self-service portal redesign" className={styles.work__img2}/>
-              </a>
-              <span className={styles.scroll} aria-label="scroll"></span>
-            </li>
-            <li className={styles.work__list_item}>
-              <a href="/">
-                <p>Pet Checkout</p>
-                <img src={pet_checkout_mobile} alt="Pet Checkout" className={styles.work__img3}/>
-              </a>
-              <span className={styles.scroll} aria-label="scroll"></span>
-
-            </li>
-            <li className={styles.work__list_item}>
-              <img src={self_serve_tablet} alt="Self-service portal redesign" className={styles.work__img2}/>
-            </li>
-          </ul>
+          <Carousel></Carousel>
         </div>
     </div>
   )
