40ead65e987d73c57f04dac8e5818cd05880df5b
Evie Perren <evie.perren@simplyhealth.co.uk>
2020-09-15 20:15:53 +0100

    creating progress bar

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -4,32 +4,40 @@ import hr_project_laptop from '../../assets/images/hr-desktop.png'
 import self_serve_tablet from '../../assets/images/self-serve.png'
 import pet_checkout_mobile from '../../assets/images/pets-checkout-mobile.png'
 import pet_link_laptop from '../../assets/images/pets-link-generator.png'
+import cx from 'classnames'
 
 const Carousel = () => {
   const [ activeIndex, setActiveIndex ] = useState(0)
+  const [ progressLevel, setProgressLevel ] = useState('')
 
   const slides = [
     {
       title: 'HR Mentor Project',
       image: hr_project_laptop,
       class: styles.laptop,
+      progress: styles._25
     },
     {
       title: 'Pet Checkout',
       image: pet_checkout_mobile,
-      class: styles.mobile
+      class: styles.mobile,
+      progress: styles._50
     },
     {
       title: 'Link Generator',
       image: pet_link_laptop,
-      class: styles.laptop
+      class: styles.laptop,
+      progress: styles._75
     },
     {
       title: 'Self Service Portal Redesign',
       image: self_serve_tablet,
-      class: styles.tablet
+      class: styles.tablet,
+      progress: styles._100
+
     }
   ]
+
   function goToPreviousSlide(){
     if(activeIndex === 0){
       setActiveIndex(slides.length - 1)
@@ -38,7 +46,6 @@ const Carousel = () => {
     )
   }
   function goToNextSlide(){
-    console.log(`active index: ${activeIndex}`)
     if(activeIndex < slides.length - 1){
       setActiveIndex(activeIndex + 1)
     } 
@@ -47,13 +54,25 @@ const Carousel = () => {
     )
   }
   function checkActiveIndex (key){
-    console.log(`key is: ${key}`)
     if(activeIndex === key) {
       return true 
     }
 
   }
-  console.log(`active index: ${activeIndex}`)
+  // switch (activeIndex){
+  //   case 0: 
+  //     setProgressLevel(styles._25)
+  //     break
+  //   case 1: 
+  //     setProgressLevel(styles._50)
+  //     break
+  //   case 2:
+  //     setProgressLevel(styles._75)
+  //     break
+  //   case 3: 
+  //     setProgressLevel(styles._100)
+  //     break
+  // }
   return (
     <Fragment>
       <h3 className={styles.number}>0{activeIndex + 1}</h3>
@@ -62,12 +81,23 @@ const Carousel = () => {
         <ul className={styles.slider}>
           {slides.map((item, key) => 
             {return (
-              <li key={key} className={checkActiveIndex(key) ? styles.active : styles.not_active }>
-                <a href="/" className={styles.item}>
-                  <img src={item.image} alt={item.title} className={item.class}/>
-                  <p>{item.title}</p> 
-                </a>
-              </li>
+              <Fragment>
+                {checkActiveIndex(key) && (
+                  <li key={key}>
+                    <a href="/" className={styles.item}>
+                      <h4 className={styles.title}>{item.title}</h4> 
+                      <img src={item.image} alt={item.title} className={item.class}/>
+                    </a>
+                    <div className={styles.container}>
+                      <div>0{activeIndex + 1}</div>
+                        <div className={styles.progress_bar}>
+                          <div className={item.progress}></div>
+                        </div>
+                      <div>0{slides.length}</div>
+                    </div>
+                  </li>
+                  )}
+              </Fragment>
             )
           })}
           </ul>
